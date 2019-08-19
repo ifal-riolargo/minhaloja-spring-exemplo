f5e922f38945597582eb66f6d9a37079e31d135e
leofernandesmo <leofernandesmo@gmail.com>
2019-08-19 18:12:51 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/example/minhaloja/modelo/Cliente.java
@@ -0,0 +1,60 @@
+package com.example.minhaloja.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "O nome não pode ser vazio.")
+    @Size(min = 2, max = 50, message = "O nome precisa ter entre {min} e {max} catacteres.")
+    private String nome;
+
+    @NotBlank(message = "O endereço não pode ser vazio.")
+    private String endereco;
+    // private List<Pedido> pedidos;
+
+    public Cliente() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    // public List<Pedido> getPedidos() {
+    //     return pedidos;
+    // }
+
+    // public void setPedidos(List<Pedido> pedidos) {
+    //     this.pedidos = pedidos;
+    // }
+
+}
