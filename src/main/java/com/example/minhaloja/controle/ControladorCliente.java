af3c6429b9004bee0cb4e57c014ef9d4d7b41046
leofernandesmo <leofernandesmo@gmail.com>
2019-08-22 17:24:30 -0300

    Adicionado validador para cliente

--- src/main/java/com/example/minhaloja/controle/ControladorCliente.java
+++ src/main/java/com/example/minhaloja/controle/ControladorCliente.java
@@ -3,6 +3,7 @@ package com.example.minhaloja.controle;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
 
@@ -10,44 +11,49 @@ import com.example.minhaloja.modelo.Cliente;
 import com.example.minhaloja.repositorios.RepositorioCliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
-public class ControladorCliente{
+public class ControladorCliente {
 
     @Autowired
     RepositorioCliente repositorioCliente;
 
     @RequestMapping("/")
-    public ModelAndView index(){
+    public ModelAndView index() {
         ModelAndView retorno = new ModelAndView("index.html");
         return retorno;
     }
 
     @RequestMapping("/formulario_cliente")
-    public ModelAndView formularioCliente(){
-        ModelAndView retorno = new ModelAndView("cadastroCliente.html");
-        
-        
+    public ModelAndView formularioCliente(Cliente cliente) {
+        ModelAndView retorno = new ModelAndView("cadastroCliente.html");        
         return retorno;
     }
 
+    // @InitBinder
+    // protected void initbinder(WebDataBinder binder){
+    // binder.setValidator();
+    // }
+
     @RequestMapping("/novo_cliente")
-    public ModelAndView cadastroCliente(Cliente cliente){
-        ModelAndView retorno = new ModelAndView("index.html");
-
-        Validator validador = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<Cliente>> violacoes = validador.validate(cliente);
-
-        System.out.println(violacoes.size());
-        for (ConstraintViolation var : violacoes) {
-            System.out.println(var.getMessage());
+    public ModelAndView cadastroCliente(@Valid Cliente cliente, BindingResult bidingResult, RedirectAttributes redirect){
+        ModelAndView retorno;
+        if(bidingResult.hasErrors()){            
+            redirect.addFlashAttribute("cliente", cliente); 
+            retorno = new ModelAndView("cadastroCliente.html");
+            return retorno;
         }
-
-        repositorioCliente.save(cliente);
-        
+        retorno = new ModelAndView("redirect:/");
+        repositorioCliente.save(cliente);  
+        redirect.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso!");     
         return retorno;
     }
 
